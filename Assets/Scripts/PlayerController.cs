ad8b074b5a029ab793ba6e23b298daa2f6b99e2f
Mikkel <58807701+professorcrazy@users.noreply.github.com>
2025-05-02 09:44:46 +0200

    Removed error in PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Lav noget her
+        
     }
 
     // Update is called once per frame
