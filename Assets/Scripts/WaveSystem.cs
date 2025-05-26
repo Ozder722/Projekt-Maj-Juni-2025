78d05ec85dfdef2e084ee98d08822c1a58f90259
Ozder722 <carlosozdk@gmail.com>
2025-05-26 11:33:01 +0200

    enemy list

--- Assets/Scripts/WaveSystem.cs
+++ Assets/Scripts/WaveSystem.cs
@@ -1,10 +1,14 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class WaveSystem
 {
+    public GameObject[] enemies;
+
     public WaveSystem(GameObject enemyPrefab, Vector3 pos)
     {
         GameObject Enemy = GameObject.Instantiate(enemyPrefab, pos, Quaternion.identity);
 
     }
 }
+
